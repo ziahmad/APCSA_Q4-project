b441c3d94655e232d801847d5e1ca786f7617678
Ziahmad <Zimraan_a@outlook.com>
2022-05-14 00:58:44 -0400

    added collision on all sides

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,9 +1,14 @@
 package src.Entity;
 
 import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
 //super class or all entities
 public class Entity {
     public static int worldX, worldY;
+
+    public double absX,absY;
+    public double screenX;
+    public double screenY;
     public double speed;
 
     public BufferedImage up1, down1, right1, left1, up2, down2, right2, left2;
@@ -11,4 +16,8 @@ public class Entity {
 
     public int spriteCounter=0;
     public int spriteNum=1;
+
+    //for collision checks
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
 }
